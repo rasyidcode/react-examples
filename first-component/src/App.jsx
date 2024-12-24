27a8e2b0dbea3f948d59403e5c0e7d9f5cb398d2
rasyidcode <rasyidcode7@gmail.com>
2024-12-24 20:43:25 +0700

    add first component

---
+++ first-component/src/App.jsx
@@ -0,0 +1,56 @@
+// function Profile() {
+//   return <img src="https://i.imgur.com/MK3eW3As.jpg" alt="Katherine Johnson" />;
+// }
+
+// export default function Gallery() {
+//   return (
+//     <section>
+//       <h1>Amazing scientists</h1>
+//       <Profile />
+//       <Profile />
+//       <Profile />
+//     </section>
+//   );
+// }
+
+// export default function Profile() {
+//   return <img src="https://i.imgur.com/MK3eW3As.jpg" alt="Katherine Johnson" />;
+// }
+
+// export default function Profile() {
+//   return (
+//     <img
+//       src="https://i.imgur.com/lICfvbD.jpg"
+//       alt="Aklilu Lemma"
+//     />
+//   );
+// }
+
+// export default function Profile() {
+//   return <img src="https://i.imgur.com/jA8hHMpm.jpg" alt="Katsuko Saruhashi" />;
+// }
+
+// function Profile() {
+//   return (
+//     <img
+//       src="https://i.imgur.com/QIrZWGIs.jpg"
+//       alt="Alan L. Hart"
+//     />
+//   );
+// }
+
+// export default function Gallery() {
+//   return (
+//     <section>
+//       <h1>Amazing scientists</h1>
+//       <Profile />
+//       <Profile />
+//       <Profile />
+//     </section>
+//   );
+// }
+
+// Write your component below!
+export default function Congratulations() {
+  return <h1>Good job!</h1>
+}
