0bcddfc57e647eca778774bc112d1b410b72e913
rasyidcode <rasyidcode7@gmail.com>
2024-12-24 20:46:44 +0700

    add styles

--- first-component/src/App.jsx
+++ first-component/src/App.jsx
@@ -30,27 +30,27 @@
 //   return <img src="https://i.imgur.com/jA8hHMpm.jpg" alt="Katsuko Saruhashi" />;
 // }
 
-// function Profile() {
-//   return (
-//     <img
-//       src="https://i.imgur.com/QIrZWGIs.jpg"
-//       alt="Alan L. Hart"
-//     />
-//   );
-// }
+function Profile() {
+  return (
+    <img
+      src="https://i.imgur.com/QIrZWGIs.jpg"
+      alt="Alan L. Hart"
+    />
+  );
+}
 
-// export default function Gallery() {
-//   return (
-//     <section>
-//       <h1>Amazing scientists</h1>
-//       <Profile />
-//       <Profile />
-//       <Profile />
-//     </section>
-//   );
-// }
+export default function Gallery() {
+  return (
+    <section>
+      <h1>Amazing scientists</h1>
+      <Profile />
+      <Profile />
+      <Profile />
+    </section>
+  );
+}
 
 // Write your component below!
-export default function Congratulations() {
-  return <h1>Good job!</h1>
-}
+// export default function Congratulations() {
+//   return <h1>Good job!</h1>
+// }
