7eb801f3a851597be1eb930c2eddb244043ed36e
rasyidcode <rasyidcode7@gmail.com>
2024-12-25 12:39:04 +0700

    understanding jsx challenge

--- first-component/src/main.jsx
+++ first-component/src/main.jsx
@@ -1,7 +1,8 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 // import App from './App.jsx'
-import App from "./TodoList";
+// import App from "./TodoList";
+import App from "./Bio";
 import "./index.css";
 
 createRoot(document.getElementById("root")).render(
