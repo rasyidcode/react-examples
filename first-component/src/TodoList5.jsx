a5b82a9b87952ddea5fb780a5201b9504abf7d72
rasyidcode <rasyidcode7@gmail.com>
2024-12-25 13:20:24 +0700

    finished challenge #2

--- first-component/src/TodoList5.jsx
+++ first-component/src/TodoList5.jsx
@@ -1,5 +1,7 @@
 const person = {
-  name: "Gregorio Y. Zara",
+  name: "John Cena",
+  imageUrl:
+    "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSLD0WJi3ePhxARZFzZBxEe92XxAk4ALD_qig&s",
   theme: {
     backgroundColor: "black",
     color: "pink",
@@ -10,11 +12,7 @@ export default function TodoList() {
   return (
     <div style={person.theme}>
       <h1>{person.name}&apos;s Todos</h1>
-      <img
-        className="avatar"
-        src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSLD0WJi3ePhxARZFzZBxEe92XxAk4ALD_qig&s"
-        alt="John Cena"
-      />
+      <img className="avatar" src={person.imageUrl} alt={person.name} />
       <ul>
         <li>Improve the videophone</li>
         <li>Prepare aeronautics lectures</li>
