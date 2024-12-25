39a7db8c8f050b85ea51c6e032f130f876f30ad3
rasyidcode <rasyidcode7@gmail.com>
2024-12-25 13:23:50 +0700

    finished challenge #3

--- first-component/src/TodoList5.jsx
+++ first-component/src/TodoList5.jsx
@@ -1,7 +1,8 @@
+const baseURL = "https://encrypted-tbn0.gstatic.com";
+const imagePath = "/images";
 const person = {
   name: "John Cena",
-  imageUrl:
-    "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSLD0WJi3ePhxARZFzZBxEe92XxAk4ALD_qig&s",
+  imageQuery: "tbn:ANd9GcSLD0WJi3ePhxARZFzZBxEe92XxAk4ALD_qig&s",
   theme: {
     backgroundColor: "black",
     color: "pink",
@@ -12,7 +13,11 @@ export default function TodoList() {
   return (
     <div style={person.theme}>
       <h1>{person.name}&apos;s Todos</h1>
-      <img className="avatar" src={person.imageUrl} alt={person.name} />
+      <img
+        className="avatar"
+        src={`${baseURL}${imagePath}?q=${person.imageQuery}`}
+        alt={person.name}
+      />
       <ul>
         <li>Improve the videophone</li>
         <li>Prepare aeronautics lectures</li>
