f37eff8cb9fd9f370774a71efa368cb9b275c2c2
rasyidcode <rasyidcode7@gmail.com>
2024-12-25 12:25:57 +0700

    understanding jsx

---
+++ first-component/src/TodoList.jsx
@@ -0,0 +1,17 @@
+export default function TodoList() {
+  return (
+    <>
+      <h1>Hedy Invisible Man Todos</h1>
+      <img
+        src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSLD0WJi3ePhxARZFzZBxEe92XxAk4ALD_qig&s"
+        alt="Invisible Man"
+        className="photo"
+      />
+      <ul>
+        <li>Invent new traffic lights</li>
+        <li>Rehearse a movie scene</li>
+        <li>Improve spectrum technology</li>
+      </ul>
+    </>
+  );
+}
