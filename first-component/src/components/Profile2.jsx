e6a6e264e1b144299a986e0f8b315ce3c4bdf730
rasyidcode <rasyidcode7@gmail.com>
2024-12-25 14:03:02 +0700

    passing props summary2

--- first-component/src/components/Profile2.jsx
+++ first-component/src/components/Profile2.jsx
@@ -1,12 +1,16 @@
+/* eslint-disable react/prop-types */
 import { getImageURL } from "../utils";
 
 export default function Profile2() {
   return (
     <Card>
-      <Avatar size={100} person={{
-        name: 'John Cena',
-        imageQuery: 'tbn:ANd9GcSLD0WJi3ePhxARZFzZBxEe92XxAk4ALD_qig&s'
-      }} />
+      <Avatar
+        size={100}
+        person={{
+          name: "John Cena",
+          imageQuery: "tbn:ANd9GcSLD0WJi3ePhxARZFzZBxEe92XxAk4ALD_qig&s",
+        }}
+      />
     </Card>
   );
 }
