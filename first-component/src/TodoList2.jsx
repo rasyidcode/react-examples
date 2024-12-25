0a2cf4bb1808933e04f452978a7ddf3c66c61513
rasyidcode <rasyidcode7@gmail.com>
2024-12-25 12:48:32 +0700

    javascript in jsx summary

---
+++ first-component/src/TodoList2.jsx
@@ -0,0 +1,25 @@
+const person = {
+  name: "Invisible Man",
+  theme: {
+    backgroundColor: "navy",
+    color: "red",
+  },
+};
+
+export default function TodoList2() {
+  return (
+    <div style={person.theme}>
+      <h1>{person.name} Todos</h1>
+      <img
+        src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSLD0WJi3ePhxARZFzZBxEe92XxAk4ALD_qig&s"
+        alt="Invisible Man"
+        className="avatar"
+      />
+      <ul>
+        <li>Invent new traffic lights</li>
+        <li>Rehearse a movie scene</li>
+        <li>Improve spectrum technology</li>
+      </ul>
+    </div>
+  );
+}
