4d42639e288d82fb24df1f39c7cae2a77c16dedc
rasyidcode <rasyidcode7@gmail.com>
2024-12-27 09:04:00 +0700

    solution #1

---
+++ first-component/src/components/Gallery4.jsx
@@ -0,0 +1,64 @@
+/* eslint-disable react/prop-types */
+import { getImageUrl } from "../utils";
+
+function Profile({
+  name,
+  imageId,
+  profession,
+  awards,
+  discovered,
+  imageSize = 70,
+}) {
+  return (
+    <section className="profile">
+      <h2>{name}</h2>
+      <img
+        className="avatar"
+        src={getImageUrl(imageId)}
+        alt={name}
+        width={imageSize}
+        height={imageSize}
+      />
+      <ul>
+        <li>
+          <b>Profession: </b>
+          {profession}
+        </li>
+        <li>
+          <b>Awards: {awards.length} </b>({awards.join(", ")})
+        </li>
+        <li>
+          <b>Discovered: </b>
+          {discovered}
+        </li>
+      </ul>
+    </section>
+  );
+}
+
+export default function Gallery4() {
+  return (
+    <div>
+      <h1>Notable Scientists</h1>
+      <Profile
+        name="Maria Skłodowska-Curie"
+        imageId="szV5sdG"
+        profession="physicist and chemist"
+        awards={[
+          "Nobel Prize in Physics",
+          "Nobel Prize in Chemistry",
+          "Davy Medal",
+          "Matteucci Medal",
+        ]}
+        discovered="polonium (chemical element)"
+      />
+      <Profile
+        name="Katsuko Saruhashi"
+        imageId="YfeOqp2"
+        profession="geochemist"
+        awards={["Miyake Prize for geochemistry", "Tanaka Prize"]}
+        discovered="a method for measuring carbon dioxide in seawater"
+      />
+    </div>
+  );
+}
