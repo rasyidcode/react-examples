d3e26710300ae657dcb9db91ab295ef249788806
rasyidcode <rasyidcode7@gmail.com>
2024-12-30 17:32:24 +0700

    another solution #3

--- first-component/src/main.jsx
+++ first-component/src/main.jsx
@@ -1,6 +1,6 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import App from "./components/Profile6";
+import App from "./components/Profile7";
 import "./index.css";
 
 createRoot(document.getElementById("root")).render(
