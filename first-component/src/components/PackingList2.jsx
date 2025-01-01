a22f0f27282d6caa680fadce3a217be4a14959f1
rasyidcode <rasyidcode7@gmail.com>
2025-01-01 07:23:16 +0700

    conditionally returning nothing with null

--- first-component/src/components/PackingList2.jsx
+++ first-component/src/components/PackingList2.jsx
@@ -1,7 +1,7 @@
 /* eslint-disable react/prop-types */
 function Item({ name, isPacked }) {
   if (isPacked) {
-    return <li className="item">{name} ✅</li>;
+    return null;
   }
   return <li className="item">{name}</li>;
 }
