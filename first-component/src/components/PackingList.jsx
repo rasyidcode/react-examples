b18a7fbaa37031d26a18ea7c6f8db5f4959a5a7c
rasyidcode <rasyidcode7@gmail.com>
2025-01-01 07:17:01 +0700

    prolog conditional rendering

---
+++ first-component/src/components/PackingList.jsx
@@ -0,0 +1,21 @@
+/* eslint-disable react/prop-types */
+function Item({ name, isPacked }) {
+  return (
+    <li className="item">
+      {name} {isPacked && "✅"}
+    </li>
+  );
+}
+
+export default function PackingList() {
+  return (
+    <section>
+      <h1>Sally Ride&apos;s Packing List</h1>
+      <ul>
+        <Item isPacked={true} name="Space suit" />
+        <Item isPacked={true} name="Helmet with a golden leaf" />
+        <Item isPacked={false} name="Photo of Tam" />
+      </ul>
+    </section>
+  );
+}
