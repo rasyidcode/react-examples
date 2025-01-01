73cde50f91ecef24739cac2309547ed5632e575d
rasyidcode <rasyidcode7@gmail.com>
2025-01-01 07:22:17 +0700

    conditionally returning jsx

---
+++ first-component/src/components/PackingList2.jsx
@@ -0,0 +1,20 @@
+/* eslint-disable react/prop-types */
+function Item({ name, isPacked }) {
+  if (isPacked) {
+    return <li className="item">{name} ✅</li>;
+  }
+  return <li className="item">{name}</li>;
+}
+
+export default function PackingList2() {
+  return (
+    <section>
+      <h1>Sally Ride&apos;s Packing List</h1>
+      <ul>
+        <Item isPacked={true} name="Space suit" />
+        <Item isPacked={true} name="Helmet with a golden leaf" />
+        <Item isPacked={false} name="Photo of Tam" />
+      </ul>
+    </section>
+  );
+}
