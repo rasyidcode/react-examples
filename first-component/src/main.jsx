b986ddda87eca6feaa6b9005fa7b937d0cac1890
rasyidcode <rasyidcode7@gmail.com>
2025-01-04 20:50:29 +0700

    solution #1b

--- first-component/src/main.jsx
+++ first-component/src/main.jsx
@@ -1,6 +1,6 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import App from "./components/ScientistList4";
+import App from "./components/ScientistList5";
 import "./index.css";
 
 createRoot(document.getElementById("root")).render(
