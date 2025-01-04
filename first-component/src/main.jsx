003365add4802a679257b194e1b476caeb07f632
rasyidcode <rasyidcode7@gmail.com>
2025-01-04 15:28:44 +0700

    done challenge #2

--- first-component/src/main.jsx
+++ first-component/src/main.jsx
@@ -1,6 +1,6 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import App from "./components/PackingList3";
+import App from "./components/PackingList4";
 import "./index.css";
 
 createRoot(document.getElementById("root")).render(
