046c856eacafe698d67f90b7bf530a61d027ed22
rasyidcode <rasyidcode7@gmail.com>
2025-01-04 18:56:42 +0700

    rendering list summary

--- first-component/src/main.jsx
+++ first-component/src/main.jsx
@@ -1,6 +1,6 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import App from "./components/DrinkList2";
+import App from "./components/ScientistList";
 import "./index.css";
 
 createRoot(document.getElementById("root")).render(
