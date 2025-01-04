bebb62bca14c8d4bb3a758e603eed4a3c39fbc10
rasyidcode <rasyidcode7@gmail.com>
2025-01-04 20:53:14 +0700

    solution #1c

---
+++ first-component/src/components/ScientistList6.jsx
@@ -0,0 +1,47 @@
+/* eslint-disable react/prop-types */
+import { people } from "../data";
+import { getImageUrl2 } from "../utils";
+
+const chemists = [];
+const everyoneElse = [];
+people.forEach((person) => {
+  if (person.profession === "chemist") {
+    chemists.push(person);
+  } else {
+    everyoneElse.push(person);
+  }
+});
+
+function ListSection({ title, people }) {
+  return (
+    <>
+      <h2>{title}</h2>
+      <ul>
+        {people.map((person) => (
+          <li key={person.id}>
+            <img src={getImageUrl2(person)} alt={person.name} />
+            <p>
+              <b>{person.name}</b>
+              {" " + person.profession + " "}
+              known for {person.accomplishment}
+            </p>
+          </li>
+        ))}
+      </ul>
+    </>
+  );
+}
+
+export default function List() {
+  const chemists = people.filter((person) => person.profession === "chemist");
+  const everyoneElse = people.filter(
+    (person) => person.profession !== "chemist"
+  );
+  return (
+    <article>
+      <h1>Scientiests</h1>
+      <ListSection title="Chemist" people={chemists} />
+      <ListSection title="Everyone Else" people={everyoneElse} />
+    </article>
+  );
+}
