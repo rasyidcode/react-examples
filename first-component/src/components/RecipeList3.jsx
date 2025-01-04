ca7542bc2e828b8f1d15f37bae295d35436f30a1
rasyidcode <rasyidcode7@gmail.com>
2025-01-04 21:13:50 +0700

    challenge #2

---
+++ first-component/src/components/RecipeList3.jsx
@@ -0,0 +1,31 @@
+/* eslint-disable react/prop-types */
+import { recipes } from "../data";
+
+function Recipe({ id, name, ingredients }) {
+  return (
+    <div key={id}>
+      <h2>{name}</h2>
+      <ul>
+        {ingredients.map((ingredient) => (
+          <li key={ingredient}>{ingredient}</li>
+        ))}
+      </ul>
+    </div>
+  );
+}
+
+export default function RecipeList() {
+  return (
+    <div>
+      <h1>RecipeList</h1>
+      {recipes.map((recipe) => (
+        <Recipe
+          key={recipe.id}
+          id={recipe.id}
+          name={recipe.name}
+          ingredients={recipe.ingredients}
+        />
+      ))}
+    </div>
+  );
+}
--- first-component/src/main.jsx
+++ first-component/src/main.jsx
@@ -1,6 +1,6 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import App from "./components/RecipeList2";
+import App from "./components/RecipeList3";
 import "./index.css";
 
 createRoot(document.getElementById("root")).render(
