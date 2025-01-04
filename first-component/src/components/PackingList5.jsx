07757b0e59e8f7ec35fd9a6a8a4dd054aaa339d4
rasyidcode <rasyidcode7@gmail.com>
2025-01-04 15:30:44 +0700

    done solution #2

---
+++ first-component/src/components/PackingList5.jsx
@@ -0,0 +1,23 @@
+/* eslint-disable react/prop-types */
+function Item({ name, importance }) {
+  return (
+    <li className="item">
+      {name}
+      {importance > 0 && " "}
+      {importance > 0 && <i>(Importance: {importance})</i>}
+    </li>
+  );
+}
+
+export default function PackingList() {
+  return (
+    <section>
+      <h1>Sally Ride&apos;s Packing List</h1>
+      <ul>
+        <Item importance={9} name="Space suit" />
+        <Item importance={0} name="Helmet with a golden leaf" />
+        <Item importance={6} name="Photo of Tam" />
+      </ul>
+    </section>
+  );
+}
