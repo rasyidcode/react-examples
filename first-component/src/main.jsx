19a6ae2d60c6f3cabd062da4e0c16785967fc65f
rasyidcode <rasyidcode7@gmail.com>
2025-01-04 15:54:15 +0700

    done solution #3b

--- first-component/src/main.jsx
+++ first-component/src/main.jsx
@@ -1,6 +1,6 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import App from "./components/DrinkList";
+import App from "./components/DrinkList2";
 import "./index.css";
 
 createRoot(document.getElementById("root")).render(
