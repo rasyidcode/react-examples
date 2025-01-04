634c07f3b1dd0af8ebd5a299e24a3eb875f105b3
rasyidcode <rasyidcode7@gmail.com>
2025-01-04 20:27:27 +0700

    add key to each item of the list

--- first-component/src/components/ScientistList2.jsx
+++ first-component/src/components/ScientistList2.jsx
@@ -1,14 +1,17 @@
-import { people } from '../data'
-import { getImageUrl2 } from '../utils'
+import { people } from "../data";
+import { getImageUrl2 } from "../utils";
+
 export default function List() {
-  const chemists = people.filter((people) => people.profession === "chemist");
-  const listItems = chemists.map((person) => <li>
-    <img src={getImageUrl2(person)} alt={person.name} />
-    <p>
+//   const chemists = people.filter((people) => people.profession === "chemist");
+  const listItems = people.map((person) => (
+    <li key={person.id}>
+      <img src={getImageUrl2(person)} alt={person.name} />
+      <p>
         <b>{person.name}</b>
-        {' ' + person.profession + ' '}
+        {" " + person.profession + " "}
         known for {person.accomplishment}
-    </p>
-  </li>);
+      </p>
+    </li>
+  ));
   return <ul>{listItems}</ul>;
 }
