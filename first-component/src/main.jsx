fb41bf1b8f1b68bba0d809df5cfab86c6a82917e
rasyidcode <rasyidcode7@gmail.com>
2025-01-04 21:23:26 +0700

    challenge #4

--- first-component/src/main.jsx
+++ first-component/src/main.jsx
@@ -1,6 +1,6 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import App from "./components/RecipeList4";
+import App from "./components/Poem";
 import "./index.css";
 
 createRoot(document.getElementById("root")).render(
