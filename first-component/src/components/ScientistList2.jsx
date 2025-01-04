c4791aea682da5f884767eb6042b178918344292
rasyidcode <rasyidcode7@gmail.com>
2025-01-04 19:15:11 +0700

    rendering list with complex data structure

--- first-component/src/components/ScientistList2.jsx
+++ first-component/src/components/ScientistList2.jsx
@@ -1,12 +1,14 @@
-const people = [
-  "Creola Katherine Johnson: mathematician",
-  "Mario José Molina-Pasquel Henríquez: chemist",
-  "Mohammad Abdus Salam: physicist",
-  "Percy Lavon Julian: chemist",
-  "Subrahmanyan Chandrasekhar: astrophysicist",
-];
-
+import { people } from '../data'
+import { getImageUrl2 } from '../utils'
 export default function List() {
-  const listItems = people.map((person) => <li>{person}</li>);
+  const chemists = people.filter((people) => people.profession === "chemist");
+  const listItems = chemists.map((person) => <li>
+    <img src={getImageUrl2(person)} alt={person.name} />
+    <p>
+        <b>{person.name}</b>
+        {' ' + person.profession + ' '}
+        known for {person.accomplishment}
+    </p>
+  </li>);
   return <ul>{listItems}</ul>;
 }
