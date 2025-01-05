ef5ac9fad4753b35730735f9380be79b6f9742eb
rasyidcode <rasyidcode7@gmail.com>
2025-01-05 13:39:19 +0700

    done challenge #2

---
+++ first-component/src/components/Panel.jsx
@@ -0,0 +1,15 @@
+/* eslint-disable react/prop-types */
+import { useState } from "react";
+
+export default function Panel({ children }) {
+  const [open, setOpen] = useState(true);
+
+  return (
+    <section className="panel">
+      <button onClick={() => setOpen(!open)}>
+        {open ? "Collapse" : "Expand"}
+      </button>
+      {open && children}
+    </section>
+  );
+}
---
+++ first-component/src/components/ProfileSection.jsx
@@ -0,0 +1,20 @@
+import Profile from "./Profile8";
+
+export default function ProfileSection() {
+  return (
+    <>
+      <Profile
+        person={{
+          imageId: "lrWQx8l",
+          name: "Subrahmanyan Chandrasekhar",
+        }}
+      />
+      <Profile
+        person={{
+          imageId: "MK3eW3A",
+          name: "Creola Katherine Johnson",
+        }}
+      />
+    </>
+  );
+}
