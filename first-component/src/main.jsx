1fa7cbc5e6a459abd59a8bc5ae81ed2442db7e04
rasyidcode <rasyidcode7@gmail.com>
2025-01-05 04:43:17 +0000

    solution #1

--- first-component/src/main.jsx
+++ first-component/src/main.jsx
@@ -1,6 +1,6 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import App from "./components/Clock2";
+import App from "./components/Clock3";
 import "./index.css";
 
 createRoot(document.getElementById("root")).render(
