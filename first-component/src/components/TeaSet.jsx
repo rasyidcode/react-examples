ce6b22516b749ede69e6c8114bbccc2851d1244a
rasyidcode <rasyidcode7@gmail.com>
2025-01-05 10:27:30 +0700

    done with keeping components pure

--- first-component/src/components/TeaSet.jsx
+++ first-component/src/components/TeaSet.jsx
@@ -1,17 +1,14 @@
-let guest = 0
-
-function Cup() {
-    // Bad: changing a preexisting variable!
-    guest = guest + 1
+/* eslint-disable react/prop-types */
+function Cup({ guest }) {
     return <h2>Tea cup for guest #{guest}</h2>
 }
 
 export default function TeaSet() {
   return (
     <>
-        <Cup />
-        <Cup />
-        <Cup />
+        <Cup guest={1} />
+        <Cup guest={2} />
+        <Cup guest={3} />
     </>
   )
 }
--- first-component/src/main.jsx
+++ first-component/src/main.jsx
@@ -1,6 +1,6 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import App from "./components/TeaSet";
+import App from "./components/TeaGathering";
 import "./index.css";
 
 createRoot(document.getElementById("root")).render(
