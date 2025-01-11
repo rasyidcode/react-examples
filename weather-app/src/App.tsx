98993bda9361d453f76847e6a10b0db91dff8aaf
rasyidcode <rasyidcode7@gmail.com>
2025-01-12 00:12:34 +0700

    weather-app: update css

--- weather-app/src/App.tsx
+++ weather-app/src/App.tsx
@@ -1,4 +1,3 @@
-// import { useState } from "react";
 import { faFrown } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { Oval } from "react-loader-spinner";
@@ -6,6 +5,7 @@ import "./App.css";
 import { KeyboardEvent, useState } from "react";
 import axios from "axios";
 import { Weather } from "./Types";
+import { getTodayString } from "./utils";
 
 type WeatherState = {
   loading: boolean;
@@ -27,13 +27,13 @@ export default function App() {
       setInput("");
       setWeather({ ...weather, loading: true });
       const url = "https://api.openweathermap.org/data/2.5/weather";
-      const api_key = import.meta.env.VITE_OPENWEATHERMAP_API_KEY;
+      const apiKey = import.meta.env.VITE_OPENWEATHERMAP_API_KEY;
       await axios
         .get(url, {
           params: {
             q: input,
             units: "metric",
-            appid: api_key,
+            appid: apiKey,
           },
         })
         .then((res) => {
@@ -48,89 +48,53 @@ export default function App() {
     }
   };
 
-  const getTodayString = () => {
-    const months = [
-      "January",
-      "February",
-      "March",
-      "April",
-      "May",
-      "June",
-      "July",
-      "August",
-      "September",
-      "October",
-      "November",
-      "December",
-    ];
-    const weekDays = [
-      "Sunday",
-      "Monday",
-      "Tuesday",
-      "Wednesday",
-      "Thursday",
-      "Friday",
-      "Saturday",
-    ];
-    const currentDate = new Date();
-    const date = `${weekDays[currentDate.getDay()]} ${currentDate.getDate()} ${
-      months[currentDate.getMonth()]
-    }`;
-    return date;
-  };
-
   return (
-    <div className="App">
-      <h1 className="app-name">Weather App</h1>
+    <div className="app">
+      <h1 className="app-title">Weather App</h1>
       <div className="search-bar">
         <input
           type="text"
-          className="city-search"
           name="query"
           placeholder="Enter city name..."
           onChange={(e) => setInput(e.target.value)}
           onKeyUp={search}
         />
       </div>
-      {weather.loading && (
-        <>
-          <br />
-          <br />
-          <Oval visible={true} />
-        </>
-      )}
-      {weather.error && (
-        <>
-          <br />
-          <br />
+      <div className="content">
+        {weather.loading && (
+          <Oval visible={true} width={50} height={50} wrapperClass="loading" />
+        )}
+        {weather.error && (
           <span className="error-message">
             <FontAwesomeIcon icon={faFrown} />
-            <span style={{ fontSize: "20px" }}>City not found</span>
+            <span style={{ fontSize: "20px", marginLeft: "1rem" }}>
+              City not found
+            </span>
           </span>
-        </>
-      )}
-      {weather && weather.data && weather.data.main && (
-        <div>
-          <div className="city-name">
-            <h2>
-              {weather.data.name}, {weather.data.sys.country}
-            </h2>
+        )}
+        {weather && weather.data && weather.data.main && (
+          <div>
+            <div className="city-name">
+              <h2>
+                {weather.data.name}, {weather.data.sys.country}
+              </h2>
+            </div>
+            <div className="date">{getTodayString()}</div>
+            <div className="icon-temp">
+              <img
+                src={`https://openweathermap.org/img/wn/${weather.data.weather[0].icon}@2x.png`}
+                alt={weather.data.weather[0].description}
+              />
+              {Math.round(weather.data.main.temp)}
+              <sup className="deg">°C</sup>
+            </div>
+            <div className="des-wind">
+              <p>{weather.data.weather[0].description.toUpperCase()}</p>
+              <p>Wind Speed: {weather.data.wind.speed}m/s</p>
+            </div>
           </div>
-          <div className="date">{getTodayString()}</div>
-          <div className="icon-temp">
-            <img
-              src={`https://openweathermap.org/img/wn/${weather.data.weather[0].icon}@2x.png`}
-              alt={weather.data.weather[0].description}
-            />
-            {Math.round(weather.data.main.temp)}
-            <sup className="deg">°C</sup>
-          </div>
-          <div className="des-wind">
-            <p>{weather.data.weather[0].description.toUpperCase()}</p>
-            <p>Wind Speed: {weather.data.wind.speed}m/s</p>
-          </div>
-        </div>
-      )}
+        )}
+      </div>
     </div>
   );
 }
---
+++ weather-app/src/utils.ts
@@ -0,0 +1,30 @@
+export const getTodayString = () => {
+  const months = [
+    "January",
+    "February",
+    "March",
+    "April",
+    "May",
+    "June",
+    "July",
+    "August",
+    "September",
+    "October",
+    "November",
+    "December",
+  ];
+  const weekDays = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  const currentDate = new Date();
+  const date = `${weekDays[currentDate.getDay()]} ${currentDate.getDate()} ${
+    months[currentDate.getMonth()]
+  }`;
+  return date;
+};
