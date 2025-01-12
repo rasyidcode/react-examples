6f62728329f024bad71a24ca96979b4f5aeaca6b
rasyidcode <rasyidcode7@gmail.com>
2025-01-12 16:53:02 +0700

    adding-interactivity: adding event handlers - 3

--- adding-interactivity/src/AI_1_App.tsx
+++ adding-interactivity/src/AI_1_App.tsx
@@ -5,9 +5,13 @@ export default function App() {
 }
 
 function Button() {
-  function handleClick() {
-    alert("You clicked me!");
-  }
-
-  return <button onClick={handleClick}>Click me</button>;
+  return (
+    <button
+      onClick={function handleClick() {
+        alert("You clicked me!");
+      }}
+    >
+      Click me
+    </button>
+  );
 }
