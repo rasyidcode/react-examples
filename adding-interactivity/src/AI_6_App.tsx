6980eda0a60cc19ceb27374592c58df3970c7a6c
rasyidcode <rasyidcode7@gmail.com>
2025-01-12 21:02:25 +0700

    adding-interactivity: Stop propagation - 1

---
+++ adding-interactivity/src/AI_6_App.tsx
@@ -0,0 +1,35 @@
+import { ReactNode } from "react";
+import "./AI_6_App.css";
+
+function Button({
+  onClick,
+  children,
+}: {
+  onClick: () => void;
+  children: ReactNode;
+}) {
+  return (
+    <button
+      onClick={(e) => {
+        e.stopPropagation();
+        onClick();
+      }}
+    >
+      {children}
+    </button>
+  );
+}
+
+export default function App() {
+  return (
+    <div
+      className="Toolbar"
+      onClick={() => {
+        alert("You clicked on the toolbar!");
+      }}
+    >
+      <Button onClick={() => alert("Playing!")}>Play Movie</Button>
+      <Button onClick={() => alert("Uploading!")}>Upload Image</Button>
+    </div>
+  );
+}
--- adding-interactivity/src/main.tsx
+++ adding-interactivity/src/main.tsx
@@ -1,7 +1,7 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
-import App from "./AI_5_App.tsx";
+import App from "./AI_6_App.tsx";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
