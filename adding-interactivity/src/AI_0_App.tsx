7d7efc3a2f175efa25f663f0fb45ffd2e2b605b7
rasyidcode <rasyidcode7@gmail.com>
2025-01-12 16:30:46 +0700

    adding-interactivity: adding section 0

---
+++ adding-interactivity/src/AI_0_App.tsx
@@ -0,0 +1,33 @@
+import { MouseEventHandler, ReactNode } from "react";
+import "./AI_0_App.css";
+
+export default function App() {
+  return (
+    <Toolbar onPlayMovie={() => alert('Playing')} onUploadImage={() => alert('Uploading')} />
+  )
+}
+
+function Toolbar({ onPlayMovie, onUploadImage }: {
+  onPlayMovie: MouseEventHandler<HTMLButtonElement>
+  onUploadImage: MouseEventHandler<HTMLButtonElement>
+}) {
+  return (
+    <div>
+      <Button onClick={onPlayMovie}>
+        Play Movie
+      </Button>
+      <Button onClick={onUploadImage}>
+        Upload Image
+      </Button>
+    </div>
+  )
+}
+
+function Button({ onClick, children }: {
+  onClick: MouseEventHandler<HTMLButtonElement>,
+  children: ReactNode
+}) {
+  return <button onClick={onClick}>
+    {children}
+  </button>
+}
