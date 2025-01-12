47ef50024512de920a294a4ab0ef714ae5ebf6d1
rasyidcode <rasyidcode7@gmail.com>
2025-01-12 16:38:21 +0700

    adding-interactivity: adding event handlers - 1

---
+++ adding-interactivity/src/AI_1_App.tsx
@@ -0,0 +1,11 @@
+import "./AI_1_App.css";
+
+export default function App() {
+  return <Button />;
+}
+
+function Button() {
+  return (
+    <button>I don't do anything</button>
+  )
+}
--- adding-interactivity/src/main.tsx
+++ adding-interactivity/src/main.tsx
@@ -1,7 +1,7 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './AI_0_App.tsx'
+import App from './AI_1_App.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
