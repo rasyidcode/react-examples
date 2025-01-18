a78d56fd2f8b6d8fafcdcdca73a155e7720b0692
rasyidcode <rasyidcode7@gmail.com>
2025-01-18 11:34:12 +0700

    adding-interactivity: challenge #1

---
+++ adding-interactivity/src/AI_8_App.tsx
@@ -0,0 +1,13 @@
+import "./AI_8_App.css";
+
+export default function LightSwitch() {
+  function handleClick() {
+    const bodyStyle = document.body.style;
+    if (bodyStyle.backgroundColor === "black") {
+      bodyStyle.backgroundColor = "white";
+    } else {
+      bodyStyle.backgroundColor = "black";
+    }
+  }
+  return <button onClick={handleClick}>Toggle the lights</button>;
+}
--- adding-interactivity/src/main.tsx
+++ adding-interactivity/src/main.tsx
@@ -1,7 +1,7 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
-import App from "./AI_7_App.tsx";
+import App from "./AI_8_App.tsx";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
