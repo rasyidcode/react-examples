7451faeabb1bbd1ef0513332eaf8987e0a90084c
rasyidcode <rasyidcode7@gmail.com>
2025-01-23 16:27:50 +0700

    adding-interactivity: state a component's memory - solution #1

---
+++ adding-interactivity/src/AI_12_App.tsx
@@ -0,0 +1,50 @@
+import { useState } from "react";
+import "./AI_11_App.css";
+import { sculptureList } from "./data";
+
+export default function Gallery() {
+  const [index, setIndex] = useState(0);
+  const [showMore, setShowMore] = useState(false);
+  const hasPrevious = index > 0;
+  const hasNext = index < sculptureList.length - 1;
+
+  function handleNextClick() {
+    if (hasNext) {
+      setIndex(index + 1);
+    } else {
+      setIndex(0);
+    }
+  }
+
+  function handlePreviousClick() {
+    if (hasPrevious) {
+      setIndex(index - 1);
+    } else {
+      setIndex(sculptureList.length - 1);
+    }
+  }
+
+  function handleMoreClick() {
+    setShowMore(!showMore);
+  }
+
+  const sculpture = sculptureList[index];
+  return (
+    <section>
+      <button onClick={handlePreviousClick}>Previous</button>
+      <button onClick={handleNextClick}>Next</button>
+      <h2>
+        <i>{sculpture.name} </i>
+        by {sculpture.artist}
+      </h2>
+      <h3>
+        ({index + 1} of {sculptureList.length})
+      </h3>
+      <button onClick={handleMoreClick}>
+        {showMore ? "Hide" : "Show"} details
+      </button>
+      {showMore && <p>{sculpture.description}</p>}
+      <img src={sculpture.url} alt={sculpture.alt} />
+    </section>
+  );
+}
