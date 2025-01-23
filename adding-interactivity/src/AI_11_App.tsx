d2079b221436c78fcf4c14ae58f1adec350c455e
rasyidcode <rasyidcode7@gmail.com>
2025-01-23 14:43:31 +0700

    adding-interactivity: giving component multiple state variables

--- adding-interactivity/src/AI_11_App.tsx
+++ adding-interactivity/src/AI_11_App.tsx
@@ -4,9 +4,14 @@ import { sculptureList } from "./data";
 
 export default function App() {
   const [index, setIndex] = useState(0);
+  const [showMore, setShowMore] = useState(false);
 
   function handleClick() {
     setIndex(index + 1);
+  }
+
+  function handleMoreClick() {
+    setShowMore(!showMore);
   }
 
   const sculpture = sculptureList[index];
@@ -20,8 +25,11 @@ export default function App() {
       <h3>
         ({index + 1} of {sculptureList.length})
       </h3>
+      <button onClick={handleMoreClick}>
+        {showMore ? "Hide" : "Show"} details
+      </button>
+      {showMore && <p>{sculpture.description}</p>}
       <img src={sculpture.url} alt={sculpture.alt} />
-      <p>{sculpture.description}</p>
     </>
   );
 }
