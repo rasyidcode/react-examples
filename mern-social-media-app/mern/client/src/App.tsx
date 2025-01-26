d912f0376ab4c5b3b11c7fe1a4bf70dca78dc7cb
rasyidcode <rasyidcode7@gmail.com>
2025-01-26 23:12:20 +0700

    mern-social-media-app: done login page ui

--- mern-social-media-app/mern/client/src/App.tsx
+++ mern-social-media-app/mern/client/src/App.tsx
@@ -1,15 +1,5 @@
+import LoginPage from "./pages/login/LoginPage";
+
 export default function App() {
-  return (
-    <div className=" bg-sky-50 h-screen flex justify-center items-center">
-      <div className=" max-w-7xl w-full flex">
-        <div className=" flex-1">
-          <h3 className=" text-5xl font-extrabold text-blue-500">Mernsocial</h3>
-          <p className=" mt-1 text-2xl">
-            Connect with friends and the world around you on Mernsocial.
-          </p>
-        </div>
-        <div className="flex-1">Right</div>
-      </div>
-    </div>
-  );
+  return <LoginPage />;
 }
